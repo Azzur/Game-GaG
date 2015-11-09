d60257cad95b84024e8a243cd6ad5fd8d3634325
Nenex <yann.schill@gmx.de>
2015-11-09 10:16:20 +0100

    clean version

---
+++ GameGaG/src/main/java/com/gamegag/GameGaGApplication.java
@@ -0,0 +1,12 @@
+package com.gamegag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameGaGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameGaGApplication.class, args);
+    }
+}
---
+++ GameGaG/src/main/java/com/gamegag/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gamegag;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GameGaGApplication.class);
+	}
+
+}
